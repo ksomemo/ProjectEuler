1ecb95dcded2065d3aae3f7e54fc7596e05793d1
k_somemo <kntsmy@hotmail.co.jp>
2013-12-20 12:13:12 +0900

    phpで問題5の回答作成した。

--- php/progblem/p005.php
+++ php/progblem/p005.php
@@ -1,19 +1,38 @@
 <?php
-$divMax = 10;
-$num    = $divMax;
-while (true) {
-	$isMod0All = true;
-	for ($i = 2; $i <= $divMax; $i++) {
-		if ($num % $i !== 0) {
-			$isMod0All = false;
+$divMax = 20;
+
+// 2-20までの因数を求める
+$factors = array();
+for ($i = 2; $i <= $divMax; $i++) {
+	$tmp = $i;
+	for ($divisor = 2; $divisor <= $tmp; $divisor++) {
+		while ($tmp % $divisor === 0) {
+			if (isset($factors[$i][$divisor])){
+				$factors[$i][$divisor]++;
+			} else {
+				$factors[$i][$divisor] = 1;
+			}
+
+			$tmp = $tmp / $divisor;
 		}
 	}
-
-	if ($isMod0All) {
-		break;
-	}
-
-	$num++;
 }
 
-echo $num . PHP_EOL;
+// 各因数の中で一番多い数を求めてそれを掛ければよい
+$f = array();
+foreach ($factors as $v) {
+	foreach ($v as $factor => $cnt) {
+		if (!isset($f[$factor])){
+			$f[$factor] = $cnt;
+		} elseif ($f[$factor] < $cnt) {
+			$f[$factor] = $cnt;
+		}
+	}
+}
+$answer = 1;
+foreach ($f as $factor => $cnt) {
+	$answer = $answer * pow($factor, $cnt);
+}
+
+print_r($factors);
+echo $answer . PHP_EOL;
